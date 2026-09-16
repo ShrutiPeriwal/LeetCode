4ccce92014ecbe1ce39f1706fb1003725e98226b
Shruti Periwal <94784239+ShrutiPeriwal@users.noreply.github.com>
2026-09-16 12:13:46 +0530

    Time: 2 ms (31.66%), Space: 95.1 MB (60.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> nums3(nums1.begin(), nums1.end());
-        nums3.insert(nums3.end(), nums2.begin(), nums2.end());
+        int n = nums1.size(), m = nums2.size();
 
-        sort(nums3.begin(), nums3.end());
-
-        int len = nums3.size();
-        if (len % 2 == 0) {
-            return (nums3[len / 2] + nums3[len / 2-1]) / 2.0;
-        } else {
-            return (nums3[len / 2]);
+        if (n > m) {
+            return findMedianSortedArrays(nums2, nums1);
         }
+
+        int low = 0, high = n;
+        while(low <= high) {
+            int mid1 = (low + high) / 2;
+            int mid2 = (n + m + 1) / 2 - mid1;
+
+            int l1 = (mid1 == 0 ? INT_MIN : nums1[mid1 - 1]);
+            int r1 = (mid1 == n ? INT_MAX : nums1[mid1]);
+
+            int l2 = (mid2 == 0 ? INT_MIN : nums2[mid2 - 1]);
+            int r2 = (mid2 == m ? INT_MAX : nums2[mid2]);
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n + m) % 2 == 0) {
+                    return (max(l1, l2) + min(r1, r2)) / 2.0;
+                } else {
+                    return max(l1, l2);
+                }
+            }
+            if ( l1 > r2) {
+                high = mid1 - 1;
+            } else {
+                low = mid1 + 1;
+            }
+        }
+    return 0;
     }
 };
 
